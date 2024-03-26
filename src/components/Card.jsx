19ba77a88de51e593871dfced055808e1ca8243e
bryancastillo10 <bryanangelocastillo.10@gmail.com>
2024-03-27 07:03:21 +0800

    initiate card layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,10 +4,23 @@ import BestSellerList from "src/arrays/BestSeller.js";
 
 const Card = () => {
   return (
-    <div>
-      Card Section
-    </div>
-  )
+    <div className="card__main">
+    {BestSellerList.map((drinks) => {
+    return(
+        <div key={drinks.id} className="card__container">
+          <div className="card__image">
+            <img src={drinks.img} alt="bestseller drinks" />
+          </div>
+          <div className="card__content">
+            <h1>{drinks.name_en}</h1>
+            <h1>{drinks.name_zh}</h1>
+            <p></p>
+          </div>
+        </div>
+    );
+  
+  })}
+    </div>)
 }
 
 export default Card
